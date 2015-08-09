938d4fb1e5a3de377969f50b894f48bbd335b894
James Nunn <james.nunn@jbarisk.com>
2015-08-09 23:12:41 +0100

    Tests cover setting up, starting, stopping, restarting PgTest server

--- pgtest.py
+++ pgtest.py
@@ -41,7 +41,7 @@ def which(in_file):
     Returns:
         file_path - str, normalised path to file found or None if not found
     """
-    path_no_ext, ext = os.path.splitext(in_file)
+    path_no_ext, _ = os.path.splitext(in_file)
     path_with_exe = path_no_ext + '.exe'
     # Look for the exe at the path supplied
     if os.path.split(path_no_ext)[0]:
@@ -49,25 +49,23 @@ def which(in_file):
             file_path = in_file
         elif os.path.isfile(path_with_exe):
             file_path = path_with_exe
+        if is_exe(file_path):
+            return os.path.normpath(file_path)
     else:
         for path in os.environ['PATH'].split(os.pathsep):
             file_path = os.path.join(path.strip('"'), in_file)
             file_path_with_exe = os.path.join(path.strip('"'), path_with_exe)
             if os.path.isfile(file_path_with_exe):
-                file_path = file_path_with_exe
-                break
+                if is_exe(file_path_with_exe):
+                    return os.path.normpath(file_path_with_exe)
             elif os.path.isfile(file_path):
-                file_path = file_path
-                break
-    if is_exe(file_path):
-        return os.path.normpath(file_path)
+                if is_exe(file_path):
+                    return os.path.normpath(file_path)
 
-
-def locate(file_name):
     if sys.platform.startswith('win'):
         raise OSError('Not available on Windows')
     try:
-        exact_file_regex = '/' + file_name + '$'
+        exact_file_regex = '/' + in_file + '$'
         results = subprocess.check_output(['locate', '-r', exact_file_regex])
         for file_path in results.split('\n'):
             if is_exe(file_path):
@@ -77,6 +75,8 @@ def locate(file_name):
 
 
 def is_valid_port(port):
+    if not isinstance(port, int):
+        raise TypeError('Port must be of type int')
     return 1024 < port < 65535
 
 
@@ -91,10 +91,7 @@ def bind_unused_port():
 
 
 def get_exe_path(filename):
-    if sys.platform.startswith('win'):
-        pg_ctl_exe = which(filename)
-    else:
-        pg_ctl_exe = locate(filename)
+    pg_ctl_exe = which(filename)
     if not pg_ctl_exe:
         raise IOError('File not found: {filename}'.format(filename=filename))
     else:
@@ -108,18 +105,49 @@ def str_alphanum(instr):
     return True
 
 
+def is_server_running(pg_ctl_exe, path):
+    status_cmd = pg_ctl_exe + ' status -D ' + path
+    status_proc = subprocess.Popen(status_cmd, shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+    out, err = status_proc.communicate()
+    if out and 'no server running' not in out:
+        return True
+    elif err:
+        return False
+
+
+def is_valid_cluster_dir(pg_ctl_exe, path):
+    status_cmd = pg_ctl_exe + ' status -D ' + path
+    status_proc = subprocess.Popen(status_cmd, shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+    out, err = status_proc.communicate()
+    if out and 'server' in out:
+        return True
+    elif err and 'not a database cluster directory' in err:
+        return False
+
+
 class PGTest(object):
     def __init__(self, database, username='postgres', port=None, log_file=None,
-                 no_cleanup=False, copy_data_path=None, cluster=None,
+                 no_cleanup=False, copy_data_path=None, base_dir=None,
                  pg_ctl=None):
         assert str_alphanum(database), 'Database must contain only letters and/or numbers'
         self._database = database
 
         assert str_alphanum(username), 'Username must contain only letters and/or numbers'
         self._username = username
+
+        if pg_ctl:
+            assert os.path.exists(pg_ctl), 'Executable does not exist: {path}'.format(path=pg_ctl)
+            self._pg_ctl_exe = get_exe_path(pg_ctl)
+        else:
+            self._pg_ctl_exe = get_exe_path('pg_ctl')
+
         if copy_data_path:
             assert os.path.exists(copy_data_path), 'Directory does not exist: {path}'.format(path=copy_data_path)
-            assert self._is_valid_cluster_dir(copy_data_path), 'Directory is not a database cluster directory: {path}'.format(path=copy_data_path)
+            assert is_valid_cluster_dir(self._pg_ctl_exe, copy_data_path), 'Directory is not a database cluster directory: {path}'.format(path=copy_data_path)
         self._copy_data_path = copy_data_path
 
         if port:
@@ -128,37 +156,32 @@ class PGTest(object):
         else:
             self._port = bind_unused_port()
 
-        if pg_ctl:
-            assert os.path.exists(pg_ctl), 'Executable does not exist: {path}'.format(path=pg_ctl)
-            self._pg_ctl_exe = get_exe_path(pg_ctl)
+        if base_dir:
+            assert os.path.exists(base_dir), 'Directory does not exist: {path}'.format(path=base_dir)
+            self._base_dir = base_dir
         else:
-            self._pg_ctl_exe = get_exe_path('pg_ctl')
-
-        if cluster:
-            assert os.path.exists(cluster), 'Directory does not exist: {path}'.format(path=cluster)
-            self._cluster = cluster
-        else:
-            self._cluster = tempfile.mkdtemp()
+            self._base_dir = tempfile.mkdtemp()
 
         if log_file:
             self._log_file = log_file
         else:
-            self._log_file = os.path.join(self._cluster, 'pgtest_log.txt')
+            self._log_file = os.path.join(self._base_dir, 'pgtest_log.txt')
 
-        self._data_dir = os.path.join(self._cluster, 'data')
+        self._data_dir = os.path.join(self._base_dir, 'data')
 
         if not sys.platform.startswith('win'):
-            self._listen_socket_dir = os.path.join(self._cluster, 'tmp')
+            self._listen_socket_dir = os.path.join(self._base_dir, 'tmp')
         else:
             self._listen_socket_dir = None
 
         self._no_cleanup = no_cleanup
 
         self._create_dirs()
-        self._init_cluster()
+        self._init_base_dir()
         self._set_dir_permissions()
 
     def __enter__(self):
+        self.start_server()
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
@@ -169,7 +192,7 @@ class PGTest(object):
                 'no_cleanup={!r}, copy_data_path={!r}, cluster={!r}, '
                 'pg_ctl={!r})').format(self.__class__.__name__,
                 self._database, self._username, self._port, self._log_file,
-                self._no_cleanup, self._copy_data_path, self._cluster,
+                self._no_cleanup, self._copy_data_path, self._base_dir,
                 self._pg_ctl_exe)
 
     @property
@@ -178,7 +201,7 @@ class PGTest(object):
 
     @property
     def cluster(self):
-        return self._cluster
+        return self._data_dir
 
     @property
     def log_file(self):
@@ -192,37 +215,46 @@ class PGTest(object):
     def pg_ctl(self):
         return self._pg_ctl_exe
 
+    @property
+    def url(self):
+        return 'postgresql://{user}@localhost:{port}/{db}'.format(
+                user=self._username, port=self._port, db=self._database)
 
     def start_server(self):
-        if sys.platform.startswith('win'):
-            pg_cmd = (self._pg_ctl_exe + ' start -D ' + self._data_dir +
-                      ' -l ' + self._log_file + ' -o "-F -p ' +
-                      str(self._port) + ' -d 1 -c logging_collector=off"')
-        else:
-            pg_cmd = (self._pg_ctl_exe + ' start -D ' + self._data_dir +
-                      ' -l ' + self._log_file + ' -o "-F -p ' +
-                      str(self._port) + ' -d 1 -c logging_collector=off -k ' +
-                      self._listen_socket_dir + '"')
-        subprocess.Popen(pg_cmd, shell=True)
-        self._wait_for_server_ready(10)
+        try:
+            if sys.platform.startswith('win'):
+                pg_cmd = (self._pg_ctl_exe + ' start -D ' + self._data_dir +
+                          ' -l ' + self._log_file + ' -o "-F -p ' +
+                          str(self._port) + ' -d 1 -c logging_collector=off"')
+            else:
+                pg_cmd = (self._pg_ctl_exe + ' start -D ' + self._data_dir +
+                          ' -l ' + self._log_file + ' -o "-F -p ' +
+                          str(self._port) + ' -d 1 -c logging_collector=off -k ' +
+                          self._listen_socket_dir + '"')
+            subprocess.Popen(pg_cmd, shell=True, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+            self._wait_for_server_ready(10)
+        except:
+            self.cleanup()
+            raise
         self._create_database()
 
     def stop_server(self):
         stop_cmd = self._pg_ctl_exe + ' stop -m fast -D ' + self._data_dir
-        stop = subprocess.Popen(stop_cmd, shell=True,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
+        stop = subprocess.Popen(stop_cmd, shell=True, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
         _, err = stop.communicate()
         if err:
+            self.cleanup()
             raise RuntimeError(err)
 
     def close(self):
         self.stop_server()
-        if not self._no_cleanup:
-            self.cleanup()
+        self.cleanup()
 
     def cleanup(self):
-        shutil.rmtree(self._cluster, ignore_errors=True)
+        if not self._no_cleanup:
+            shutil.rmtree(self._base_dir, ignore_errors=True)
 
     def dsn(self, **kwargs):
         dsn_dict = dict(kwargs)
@@ -232,37 +264,45 @@ class PGTest(object):
         dsn_dict.setdefault('database', self._database)
         return dsn_dict
 
-    def url(self):
-        return 'postgresql://{user}@localhost:{port}/{db}'.format(
-                user=self._username, port=self._port, db=self._database)
-
-    def _init_cluster(self):
-        if self._copy_data_path:
-            shutil.copytree(self._copy_data_path, self._data_dir)
-        else:
-            init_cmd = (self._pg_ctl_exe + ' initdb -D ' +
-                        self._data_dir + ' -o "-U ' +
-                        self._username + ' -A trust"')
-            init_proc = subprocess.Popen(init_cmd, shell=True,
-                                         stdout=subprocess.PIPE,
-                                         stderr=subprocess.PIPE)
-            _, err = init_proc.communicate()
-            if err:
-                raise IOError(err)
-        assert self._is_valid_cluster_dir(self._data_dir), 'Failed to create cluster: {path}'.format(path=self._data_dir)
+    def _init_base_dir(self):
+        try:
+            if self._copy_data_path:
+                shutil.copytree(self._copy_data_path, self._data_dir)
+            else:
+                init_cmd = (self._pg_ctl_exe + ' initdb -D ' +
+                            self._data_dir + ' -o "-U ' +
+                            self._username + ' -A trust"')
+                init_proc = subprocess.Popen(init_cmd, shell=True,
+                                             stdout=subprocess.PIPE,
+                                             stderr=subprocess.PIPE)
+                _, err = init_proc.communicate()
+                if err:
+                    raise IOError(err)
+            assert is_valid_cluster_dir(self._pg_ctl_exe, self._data_dir), 'Failed to create cluster: {path}'.format(path=self._data_dir)
+        except:
+            self.cleanup()
+            raise
 
     def _create_dirs(self):
-        for path in (self._cluster, self._data_dir, self._listen_socket_dir):
-            if path and not os.path.exists(path):
-                os.makedirs(path)
+        try:
+            for path in (self._base_dir, self._data_dir, self._listen_socket_dir):
+                if path and not os.path.exists(path):
+                    os.makedirs(path)
+        except:
+            self.cleanup()
+            raise
 
     def _set_dir_permissions(self):
-        for path in (self._cluster, self._data_dir, self._listen_socket_dir):
-            os.chmod(path, 0o700)
+        try:
+            for path in (self._base_dir, self._data_dir, self._listen_socket_dir):
+                os.chmod(path, 0o700)
+        except:
+            self.cleanup()
+            raise
 
     def _is_connection_available(self):
         try:
-            with closing(pg8000.connect(**self.dsn(database='postgres'))):
+            with closing(pg8000.connect(**self.dsn(database='template1'))):
                 pass
         except pg8000.Error:
             return False
@@ -280,37 +320,17 @@ class PGTest(object):
         with closing(pg8000.connect(**self.dsn(database='postgres'))) as conn:
             conn.autocommit = True
             with closing(conn.cursor()) as cursor:
-                query = 'CREATE DATABASE "{database}"'.format(database=self._database)
-                cursor.execute(query)
-
-    def _is_valid_cluster_dir(self, path):
-        status_cmd = self._pg_ctl_exe + ' status -D ' + path
-        status_proc = subprocess.Popen(status_cmd, shell=True,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
-        out, err = status_proc.communicate()
-        if out and 'server' in out:
-            return True
-        elif err and 'not a database cluster directory' in err:
-            return False
-
-    def _is_cluster_running(self, path):
-        status_cmd = self._pg_ctl_exe + ' status -D ' + path
-        status_proc = subprocess.Popen(status_cmd, shell=True,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
-        out, err = status_proc.communicate()
-        if out and 'no server running' not in out:
-            return True
-        elif err:
-            return False
+                cursor.execute("SELECT COUNT(*) FROM pg_database WHERE "
+                               "datname='{database}'".format(database=self._database))
+                if cursor.fetchone()[0] <= 0:
+                    query = 'CREATE DATABASE "{database}"'.format(database=self._database)
+                    cursor.execute(query)
 
 
 def main():
-    with PGTest('test') as pg:
-        pg.start_server()
+    with PGTest('test', no_cleanup=True) as pg:
+        print pg
 
 
 if __name__ == '__main__':
     main()
-
--- test/test.py
+++ test/test.py
@@ -1,58 +1,119 @@
 # encoding: utf-8
+import os
+import re
 import socket
+import shutil
 import sys
+import tempfile
 import unittest
+import zipfile
 
 import pgtest
 
 
-@unittest.skip
-class TestPgTest(unittest.TestCase):
+def unzip(source_filename, dest_dir):
+    zf = zipfile.ZipFile(source_filename)
+    zf.extractall(dest_dir)
+
+
+class CustomAssertions():
+
+    def assertRegexMatch(self, pattern, target, msg=None):
+        comp = re.compile(pattern)
+        if not re.match(comp, target):
+            if not msg:
+                msg = '{!r} does not match pattern {!r}'.format(target, pattern)
+            raise AssertionError(msg)
+
+    def assertFileExists(self, path):
+        if not os.path.isfile(path):
+            raise AssertionError('File does not exist: {!r}'.format(path))
+
+    def assertDirExists(self, path):
+        if not os.path.isdir(path):
+            raise AssertionError('Directory does not exist: {!r}'.format(path))
+
+    def assertFileNotExists(self, path):
+        if os.path.isfile(path):
+            raise AssertionError('File exists: {!r}'.format(path))
+
+    def assertDirNotExists(self, path):
+        if os.path.isdir(path):
+            raise AssertionError('Directory exists: {!r}'.format(path))
+
+
+class TestPgTestCleanup(unittest.TestCase, CustomAssertions):
+
+    def test_cleanup(self):
+        pg = pgtest.PGTest('test')
+        pg.start_server()
+        base_dir = pg._base_dir
+        pg.close()
+        self.assertDirNotExists(base_dir)
+
+    def test_no_cleanup(self):
+        pg = pgtest.PGTest('test', no_cleanup=True)
+        pg.start_server()
+        base_dir = pg._base_dir
+        cluster = pg.cluster
+        pg.close()
+        self.assertDirExists(base_dir)
+        self.assertDirExists(cluster)
+        shutil.rmtree(base_dir, ignore_errors=True)
+
+
+class TestPgTestCopyData(unittest.TestCase, CustomAssertions):
     pass
-    # def test_basic(self):
-    #     try:
-    #         # start postgresql server
-    #         pgsql = testing.postgresql.Postgresql()
-    #         self.assertIsNotNone(pgsql)
-    #         params = pgsql.dsn()
-    #         self.assertEqual('test', params['database'])
-    #         self.assertEqual('127.0.0.1', params['host'])
-    #         self.assertEqual(pgsql.port, params['port'])
-    #         self.assertEqual('postgres', params['user'])
 
-    #         # connect to postgresql (w/ psycopg2)
-    #         conn = psycopg2.connect(**pgsql.dsn())
-    #         self.assertIsNotNone(conn)
-    #         self.assertRegexpMatches(pgsql.read_log(), 'is ready to accept connections')
-    #         conn.close()
 
-    #         # connect to postgresql (w/ sqlalchemy)
-    #         engine = sqlalchemy.create_engine(pgsql.url())
-    #         self.assertIsNotNone(engine)
 
-    #         # connect to postgresql (w/ pg8000)
-    #         conn = pg8000.connect(**pgsql.dsn())
-    #         self.assertIsNotNone(conn)
-    #         self.assertRegexpMatches(pgsql.read_log(), 'is ready to accept connections')
-    #         conn.close()
-    #     finally:
-    #         # shutting down
-    #         pid = pgsql.pid
-    #         self.assertTrue(pid)
-    #         os.kill(pid, 0)  # process is alive
+class TestPgTestDefault(unittest.TestCase, CustomAssertions):
 
-    #         pgsql.stop()
-    #         sleep(1)
+    @classmethod
+    def setUpClass(cls):
+        cls.pg_ctl_exe = pgtest.get_exe_path('pg_ctl')
+        cls.pg = pgtest.PGTest('test')
+        cls.pg.start_server()
+        cls.base_dir = cls.pg._base_dir
 
-    #         self.assertIsNone(pgsql.pid)
-    #         with self.assertRaises(OSError):
-    #             os.kill(pid, 0)  # process is down
+    @classmethod
+    def tearDownClass(cls):
+        cls.pg.close()
+
+    def test_pg_ctl_valid(self):
+        self.assertTrue(pgtest.is_valid_port(self.pg.port))
+
+    def test_port_valid(self):
+        self.assertTrue(pgtest.get_exe_path(self.pg.pg_ctl))
+
+    def test_url_valid(self):
+        self.assertRegexMatch(r'postgresql://[\w]+@localhost:[0-9]+/[\w]+', self.pg.url)
+
+    def test_cluster_valid(self):
+        self.assertTrue(pgtest.is_valid_cluster_dir(self.pg_ctl_exe, self.pg.cluster))
+
+    def test_cluster_running(self):
+        self.assertTrue(pgtest.is_server_running(self.pg_ctl_exe, self.pg.cluster))
+
+    def test_server_stop_start(self):
+        self.assertTrue(pgtest.is_server_running(self.pg_ctl_exe, self.pg.cluster))
+        self.pg.stop_server()
+        self.assertFalse(pgtest.is_server_running(self.pg_ctl_exe, self.pg.cluster))
+        self.pg.start_server()
+        self.assertTrue(pgtest.is_server_running(self.pg_ctl_exe, self.pg.cluster))
+
+    @unittest.skipIf(sys.platform.startswith('win'), 'Unix only')
+    def test_listen_socket_dir_exists(self):
+        self.assertDirExists(self.pg._listen_socket_dir)
+
+    def test_log_file_exists(self):
+        self.assertFileExists(self.pg._log_file)
 
 
 class ModuleFunctionsTest(unittest.TestCase):
 
     def test_bind_unused_port(self):
-        for _ in xrange(1000):
+        for _ in xrange(1, 1000):
             port = pgtest.bind_unused_port()
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.assertEqual(sock.connect_ex(('localhost', port)), 111)
@@ -60,25 +121,58 @@ class ModuleFunctionsTest(unittest.TestCase):
 
     def test_is_valid_port(self):
         for i in range(1025, 65534):
-            self.assertEqual(True, pgtest.is_valid_port(i))
+            self.assertTrue(pgtest.is_valid_port(i))
 
-    def test_is_not_valid_port(self):
-        for i in range(1024):
-            self.assertEqual(False, pgtest.is_valid_port(i))
-        for i in range(65535, 100000):
-            self.assertEqual(False, pgtest.is_valid_port(i))
+    def test_is_not_valid_port_too_low(self):
+        for l in range(1024):
+            self.assertFalse(pgtest.is_valid_port(l))
 
-    def test_url(self):
-        url = 'postgresql://postgres:pass@localhost:5432/testdb'
-        self.assertEqual(url, pgtest.url('postgres', 'pass', 'localhost', 5432, 'testdb'))
+    def test_is_not_valid_port_too_high(self):
+        for u in range(65535, 100000):
+            self.assertFalse(pgtest.is_valid_port(u))
+
+    def test_is_not_valid_port_neg(self):
+        self.assertFalse(pgtest.is_valid_port(-1))
+
+    def test_is_not_valid_port_str(self):
+        with self.assertRaises(TypeError):
+            pgtest.is_valid_port('1234')
 
     def test_get_exe_path(self):
         with self.assertRaises(IOError):
             pgtest.get_exe_path('notafile')
 
-    def test_wait_for_server_timeout(self):
-        with self.assertRaises(pgtest.TimeoutError):
-            pgtest._wait_for_server_ready(0.1)
+    def test_str_alphanum_is_valid(self):
+        self.assertTrue(pgtest.str_alphanum('abcdefghijklmnopqrstuvwxyzABCDE'
+                                            'FGHIJKLMNOPQRSTUVWXYZ1234567890'))
+
+    def test_str_alphanum_is_not_valid(self):
+        for c in r'`¬!"£$%^&*()_+[]{};\'#:@~,./<>? ':
+            self.assertFalse(pgtest.str_alphanum(c))
+
+    def test_is_valid_cluster_dir(self):
+        pg_ctl_exe = pgtest.get_exe_path('pg_ctl')
+        temp_dir = tempfile.mkdtemp()
+        data_dir = os.path.join(temp_dir, 'data')
+        curr_dir = os.path.dirname(__file__)
+        unzip(os.path.join(curr_dir,'test_cluster.zip'), temp_dir)
+        self.assertTrue(pgtest.is_valid_cluster_dir(pg_ctl_exe, data_dir))
+        shutil.rmtree(temp_dir, ignore_errors=True)
+
+    def test_is_not_valid_cluster_dir(self):
+        pg_ctl_exe = pgtest.get_exe_path('pg_ctl')
+        temp_dir = tempfile.mkdtemp()
+        self.assertFalse(pgtest.is_valid_cluster_dir(pg_ctl_exe, temp_dir))
+        shutil.rmtree(temp_dir, ignore_errors=True)
+
+    def test_is_not_server_running(self):
+        pg_ctl_exe = pgtest.get_exe_path('pg_ctl')
+        temp_dir = tempfile.mkdtemp()
+        data_dir = os.path.join(temp_dir, 'data')
+        curr_dir = os.path.dirname(__file__)
+        unzip(os.path.join(curr_dir,'test_cluster.zip'), temp_dir)
+        self.assertFalse(pgtest.is_server_running(pg_ctl_exe, data_dir))
+        shutil.rmtree(temp_dir, ignore_errors=True)
 
 
 @unittest.skipIf(sys.platform.startswith('win'), 'Unix only')
@@ -96,34 +190,28 @@ class UnixFileTest(unittest.TestCase):
     def test_which_is_not_executable(self):
         self.assertEqual(None, pgtest.which('does not exist'))
 
-    def test_locate(self):
-        self.assertEqual('/bin/ping', pgtest.locate('ping'))
-
-    def test_locate_fail(self):
-        self.assertEqual(None, pgtest.locate('notafile'))
-
 
 @unittest.skipUnless(sys.platform.startswith('win'), 'Windows only')
 class WindowsFileTest(unittest.TestCase):
 
     def test_which_is_executable(self):
-        self.assertEqual('C:\\Windows\\system32\\ping.exe', pgtest.which('ping'))
+        self.assertEqual('C:\\Windows\\system32\\ping.exe',
+                         pgtest.which('ping'))
 
     def test_which_with_extension_is_executable(self):
-        self.assertEqual('C:\\Windows\\system32\\ping.exe', pgtest.which('ping.exe'))
+        self.assertEqual('C:\\Windows\\system32\\ping.exe',
+                         pgtest.which('ping.exe'))
 
     def test_which_path_is_executable(self):
-        self.assertEqual('C:\\Windows\\system32\\ping.exe', pgtest.which('C:/Windows/system32/ping'))
+        self.assertEqual('C:\\Windows\\system32\\ping.exe',
+                         pgtest.which('C:/Windows/system32/ping'))
 
     def test_which_path_with_extension_is_executable(self):
-        self.assertEqual('C:\\Windows\\system32\\ping.exe', pgtest.which('C:/Windows/system32/ping.exe'))
+        self.assertEqual('C:\\Windows\\system32\\ping.exe',
+                         pgtest.which('C:/Windows/system32/ping.exe'))
 
     def test_which_is_not_executable(self):
         self.assertEqual(None, pgtest.which('does not exist'))
-
-    def test_locate_unavailable(self):
-        with self.assertRaises(OSError):
-            pgtest.locate('ping')
 
 
 if __name__ == '__main__':

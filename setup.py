c24a8846187a2c0b1da02b95c63e46d9cba8e9a0
James Nunn <james.nunn@jbarisk.com>
2015-08-25 16:13:24 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ setup(
     name='pgtest',
     version='1.0.0',
     description=('Creates a temporary, local PostgreSQL database cluster and '
-                'server for unittesting, and cleans up after itself'),
+                 'server for unittesting, and cleans up after itself'),
     url='https://github.com/jamesnunn/pgtest',
     author='James Nunn',
     author_email='jamesnunn123 at gmail.com',
     license='MIT',
     packages=['pgtest'],
-    install_requires = ['pg8000 >= 1.10'],
-    zip_safe=False)
+    install_requires = ['pg8000 >= 1.10'])

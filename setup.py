46e77ecc8da08813a86ffcf840605a3734b57738
James Nunn <jamesnunn123@gmail.com>
2017-10-12 09:25:36 +0100

    bump ver

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pgtest',
-    version='1.0.0',
+    version='1.1.0',
     description=('Creates a temporary, local PostgreSQL database cluster and '
                  'server for unittesting, and cleans up after itself'),
     url='https://github.com/jamesnunn/pgtest',

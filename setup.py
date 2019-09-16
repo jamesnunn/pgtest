a745323ec0c2299f48ff72050dcd646fa9f293ef
Leopold Talirz <leopold.talirz@gmail.com>
2019-09-16 17:04:04 +0200

    bump version number (#17)
    prepare 1.3.1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pgtest',
-    version='1.2.0',
+    version='1.3.1',
     description=('Creates a temporary, local PostgreSQL database cluster and '
                  'server for unittesting, and cleans up after itself'),
     url='https://github.com/jamesnunn/pgtest',

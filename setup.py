7443fe2e8f3e850c813d190bf4ccda9bb906360a
Leopold Talirz <leopold.talirz@gmail.com>
2019-09-16 17:06:51 +0200

    use README for pypi description
    Use README.md file as long_description of PyPI package.

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     version='1.3.1',
     description=('Creates a temporary, local PostgreSQL database cluster and '
                  'server for unittesting, and cleans up after itself'),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/jamesnunn/pgtest',
     author='James Nunn',
     author_email='jamesnunn123@gmail.com',
